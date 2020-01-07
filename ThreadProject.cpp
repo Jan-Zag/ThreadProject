b21a1202c2813e7e845ed3b0e517b1251ca6f352
Ivan_Zagorniy <grozniy.i228@gmail.com>
2020-01-07 19:45:19 +0200

    Добавьте файлы проекта.

---
+++ ThreadProject.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void DoWork(int a, int b) //emulation of a complex task in 11 seconds (a little more)
+{
+	this_thread::sleep_for(chrono::milliseconds(3000));
+
+	cout << "============\t" << "DoWork STARTED\t============" << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+
+	cout << "a+b = " << a + b << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(3000));
+
+	cout << "============\t" << "DoWork STOPED\t============" << endl;
+}
+
+int main()
+{
+	thread th(DoWork, 2, 3);
+
+	for (size_t i = 0; true; i++)
+	{
+		cout << "ID of Thread = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));										//0.5 sec stop (task execution emulation)
+	}
+
+	th.join();
+	return 0;
+}
